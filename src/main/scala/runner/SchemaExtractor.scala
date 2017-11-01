b3cd00e0e46bbab2461dde72a7b43f73137a2b9c
Andrey Semakov <andrey.semakov@gmail.com>
2017-11-01 19:17:56 +0300

    refactor schema extractor

--- src/main/scala/runner/SchemaExtractor.scala
+++ src/main/scala/runner/SchemaExtractor.scala
@@ -8,14 +8,15 @@ import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 
 
-object ExtractorActions extends Enumeration {
-  type ExtractorActions = Value
-  val create, drop, truncate = Value
-}
-
-case class SchemaExtractorParams(action: ExtractorActions.Value = ExtractorActions.create, execute: Boolean = false)
-
 object SchemaExtractor {
+
+  object ExtractorActions extends Enumeration {
+    type ExtractorActions = Value
+    val create, drop, truncate = Value
+  }
+
+  case class SchemaExtractorParams(action: ExtractorActions.Value = ExtractorActions.create, execute: Boolean = false)
+
   implicit val extractorActionsRead: Read[ExtractorActions.Value] = scopt.Read.reads(ExtractorActions.withName)
 
   def main(args: Array[String]): Unit = {
@@ -24,6 +25,13 @@ object SchemaExtractor {
       opt[ExtractorActions.Value]('a', "action").action((x, c) => c.copy(action = x))
       opt[Unit]('x', "execute").action((x, c) => c.copy(execute = true))
     }
+    parser.parse(args, SchemaExtractorParams()) match {
+      case Some(config) => main(config)
+      case None => // arguments are bad, error message will have been displayed
+    }
+  }
+
+  protected def main(config: SchemaExtractorParams):Unit = {
     val tables = Seq(
       TableQuery[RegionReg],
       TableQuery[CategoryReg],
@@ -32,22 +40,16 @@ object SchemaExtractor {
       TableQuery[KadastrReg],
       TableQuery[PointReg]
     )
-    parser.parse(args, SchemaExtractorParams()) match {
-      case Some(config) =>
-        val statements = config.action match {
-          case ExtractorActions.create => tables.map(_.schema.create)
-          case ExtractorActions.drop => tables.map(_.schema.drop)
-          case ExtractorActions.truncate => tables.map(_.schema.truncate)
-        }
-        if (config.execute) {
-          val db = Database.forConfig("mydb")
-          Await.result(db.run(DBIO.seq(statements: _*)), Duration.Inf)
-        } else {
-          statements.flatMap(_.statements).foreach(println)
-        }
-
-      case None =>
-      // arguments are bad, error message will have been displayed
+    val statements = config.action match {
+      case ExtractorActions.create => tables.map(_.schema.create)
+      case ExtractorActions.drop => tables.map(_.schema.drop)
+      case ExtractorActions.truncate => tables.map(_.schema.truncate)
+    }
+    if (config.execute) {
+      val db = Database.forConfig("mydb")
+      Await.result(db.run(DBIO.seq(statements: _*)), Duration.Inf)
+    } else {
+      statements.flatMap(_.statements).foreach(println)
     }
   }
 }
